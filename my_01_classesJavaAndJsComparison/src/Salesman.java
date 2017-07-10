3de6bcc904950196439948ddf2ec898f658d1feb
apetazzi <petazzi63@gmail.com>
2017-07-10 14:42:04 +0200

    added class to show inheritance

---
+++ my_01_classesJavaAndJsComparison/src/Salesman.java
@@ -0,0 +1,33 @@
+public class Salesman extends Employee {
+
+	private String department;
+
+	public Salesman(String fullName, String department) {
+		super(fullName);
+
+		this.department = department;
+	}
+
+	public Salesman() {
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public static void main(String[] args) {
+		Salesman.log(); // logs "Logging..."
+
+		Salesman emp1 = new Salesman("John Doe", "pants");
+		System.out.println("emp1=" + emp1.getFullName());
+
+		Salesman emp2 = new Salesman();
+		emp2.setFullName("Jane Doe");
+		emp2.setDepartment("shirts");
+		System.out.println("emp2=" + emp2.getFullName());
+	}
+}
