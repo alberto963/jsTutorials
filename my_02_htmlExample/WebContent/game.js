fe1ad5fac705c565cef687aa2bdebb0181234e28
apetazzi <petazzi63@gmail.com>
2017-07-21 17:42:00 +0200

    game code added (part 1)

--- my_02_htmlExample/WebContent/game.js
+++ my_02_htmlExample/WebContent/game.js
@@ -1,120 +1,83 @@
 /**
  * 
  */
-
-var myGamePiece;
-var crossLines = [];
-var myScore;
-
 function startGame() {
-
-	// gameArea.start();
-	$('canvas').click(function() {
-		$(this).toggleClass(function() {
-			if ($(this).is(".empty")) {
-				return "blue";
-			} else {
-				return "red";
-			}
-		});
-	});
+	game.start();
 }
 
-var gameArea = {
-	canvas : document.createElement("canvas"),
+var game = {
+	emptyTiles : [],
+	redTiles : [],
+	greenTiles : [],
 	start : function() {
-		this.canvas.width = 310;
-		this.canvas.height = 310;
-		this.context = this.canvas.getContext("2d");
-		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-		updateGameArea();
+
+		var count = 0;
+		$("canvas").each(function() {
+			var $thisTile = $(this);
+			$thisTile.click(function() {
+
+				if ($(this).is(".red") || $(this).is(".green")) {
+					return;
+				}
+
+				count++;
+				$(this).toggleClass("green");
+
+				console.info("count=" + count)
+
+				check();
+				play();
+			});
+		});
 	},
-	clear : function() {
-		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	clear(){
+		
 	}
 }
 
-function component(width, height, color, x, y, type) {
-	this.type = type;
-	this.score = 0;
-	this.width = width;
-	this.height = height;
-	this.speedX = 0;
-	this.speedY = 0;
-	this.x = x;
-	this.y = y;
-	this.gravity = 0;
-	this.gravitySpeed = 0;
+function play() {
 
-	this.update = function() {
-		ctx = gameArea.context;
-		if (this.type == "text") {
-			ctx.font = this.width + " " + this.height;
-			ctx.fillStyle = color;
-			ctx.fillText(this.text, this.x, this.y);
-		} else {
-			ctx.fillStyle = color;
-			ctx.fillRect(this.x, this.y, this.width, this.height);
-		}
-	}
+	var emptyTiles = [];
+	$("canvas").each(function() {
 
-	this.newPos = function() {
-		this.gravitySpeed += this.gravity;
-		this.x += this.speedX;
-		this.y += this.speedY + this.gravitySpeed;
-		this.hitBottom();
-	}
-
-	this.hitBottom = function() {
-		var rockbottom = gameArea.canvas.height - this.height;
-		if (this.y > rockbottom) {
-			this.y = rockbottom;
-			this.gravitySpeed = 0;
-		}
-	}
-
-	this.crashWith = function(otherobj) {
-		var myleft = this.x;
-		var myright = this.x + (this.width);
-		var mytop = this.y;
-		var mybottom = this.y + (this.height);
-		var otherleft = otherobj.x;
-		var otherright = otherobj.x + (otherobj.width);
-		var othertop = otherobj.y;
-		var otherbottom = otherobj.y + (otherobj.height);
-		var crash = true;
-		if ((mybottom < othertop) || (mytop > otherbottom)
-				|| (myright < otherleft) || (myleft > otherright)) {
-			crash = false;
+		if ($(this).is(".red") || $(this).is(".green")) {
+			return;
 		}
 
-		return crash;
-	}
+		emptyTiles.push($(this));
+	});
+
+	console.info("emptyTiles=" + emptyTiles);
+
+	move(emptyTiles);
 }
 
-function updateGameArea() {
-	var x, y, width, height;
-
-	gameArea.clear();
-	x = gameArea.canvas.width;
-	y = gameArea.canvas.height;
-	width = x;
-	height = y;
-
+function move(emptyTiles) {
 	/*
-	 * Vertical lines
+	 * Random move
 	 */
-	crossLines.push(new component(10, height, "black", 100, 0));
-	crossLines.push(new component(10, height, "black", 200, 0));
+	var move = emptyTiles[Math.floor(Math.random() * emptyTiles.length)];
 
-	/*
-	 * Orizontal lines
-	 */
-	crossLines.push(new component(width, 10, "black", 0, 100));
-	crossLines.push(new component(width, 10, "black", 0, 200));
+	console.info("move=" + move);
 
-	for (i = 0; i < crossLines.length; i += 1) {
-		crossLines[i].update();
-	}
+	$(move).toggleClass("red");
+}
 
+function check() {
+	$("canvas").each(function() {
+
+		if ($(this).is(".red")) {
+			game.redTiles.push($(this));
+
+			return;
+		}
+
+		if ($(this).is(".green")) {
+			game.greenTiles.push($(this));
+
+			return;
+		}
+
+		game.emptyTiles.push($(this));
+	});
 }
