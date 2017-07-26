094f9bc0a743e24489c7d0571b9f4d0150c1da0c
Alberto <petazzi63@gmail.com>
2017-07-26 22:03:22 +0200

    fixed code to check correctly who wins

--- my_02_htmlExample/WebContent/game.js
+++ my_02_htmlExample/WebContent/game.js
@@ -6,9 +6,10 @@ function startGame() {
 }
 
 var game = {
-	emptyTiles : [],
-	redTiles : [],
-	greenTiles : [],
+
+	winConf : [ [ 0, 1, 2 ], [ 3, 4, 5 ], [ 6, 7, 8 ], [ 0, 3, 6 ],
+			[ 1, 4, 7 ], [ 2, 5, 8 ], [ 0, 4, 8 ], [ 2, 4, 6 ] ],
+
 	start : function() {
 
 		var count = 0;
@@ -25,34 +26,60 @@ var game = {
 
 				console.info("count=" + count)
 
-				check();
-				play();
+				var t = $(this).attr('id');
+				count = play(count, t);
 			});
 		});
 	},
-	clear(){
-		
+	clear : function() {
+
 	}
 }
 
-function play() {
+function play(count, t) {
+	var winConf = check(count, t, ".green");
+	if (winConf.length == 0) {
+		var r = move();
+
+		count++;
+
+		var winConf = check(count, r, ".red");
+
+		if (winConf.length != 0) {
+			/*
+			 * Red wins
+			 */
+			console.info("RED WINS")
+		}
+
+		return count;
+	}
+
+	/*
+	 * Green wins
+	 */
+	console.info("GREEN WINS")
+}
+
+function move() {
 
 	var emptyTiles = [];
+
 	$("canvas").each(function() {
 
-		if ($(this).is(".red") || $(this).is(".green")) {
+		if ($(this).is(".red")) {
+
+			return;
+		}
+
+		if ($(this).is(".green")) {
+
 			return;
 		}
 
 		emptyTiles.push($(this));
 	});
 
-	console.info("emptyTiles=" + emptyTiles);
-
-	move(emptyTiles);
-}
-
-function move(emptyTiles) {
 	/*
 	 * Random move
 	 */
@@ -61,23 +88,52 @@ function move(emptyTiles) {
 	console.info("move=" + move);
 
 	$(move).toggleClass("red");
+
+	var r = $(move).attr('id');
+	
+	return r
 }
 
-function check() {
-	$("canvas").each(function() {
+function check(count, t, player) {
+	var winConf = [];
 
-		if ($(this).is(".red")) {
-			game.redTiles.push($(this));
+	if (count < 5) {
+		return winConf;
+	}
 
-			return;
+	var possConf = [ [] ];
+	var t_i = parseInt(t, 10);
+	game.winConf.forEach(function(c) {
+		if (c.indexOf(t_i) != -1) {
+			possConf.push(c);
+		}
+	})
+
+	console.info("possConf=" + possConf);
+
+	possConf.some(function(c) {
+		var f = 0;
+		c.some(function(c0) {
+			/*
+			 * If it is in increase f and return false
+			 * 
+			 * If it is not in return true
+			 */
+			var found = $("#" + c0).is(player);
+			if (found) {
+				f++;
+			}
+
+			return !found;
+		})
+		var win = (f == 3);
+
+		if (win) {
+			winConf = c;
 		}
 
-		if ($(this).is(".green")) {
-			game.greenTiles.push($(this));
+		return win;
+	})
 
-			return;
-		}
-
-		game.emptyTiles.push($(this));
-	});
+	return winConf;
 }
