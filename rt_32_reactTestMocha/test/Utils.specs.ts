dc69e43dbad2af5f96ce7f7c588688b15dd7a2e2
Alberto Petazzi <petazzi63@gmail.com>
2019-10-21 16:08:28 +0200

    rt_32: chg

--- rt_32_reactTestMocha/test/Utils.specs.ts
+++ rt_32_reactTestMocha/test/Utils.specs.ts
@@ -1,21 +1,25 @@
 import { expect } from 'chai'
-import { generateData, IFunc } from '../src/Utils'
+import { t, generateData, IFunc } from '../src/Utils'
 
 // if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
 // import 'mocha'
 
+const r00 = []
+const r10 = [{x: true, y: false}]
+
 describe('Utils generateData', () => {
+    const t0 = t()
 
     it('should return empty array with boolean function', () => {
         const f: IFunc<boolean> = {x: v => true, y: v => false}
-        const r = generateData<boolean>(0, 0, f)
+        const r = generateData<boolean>(t0, 0, f)
         expect(r).to.be.an('array')
         expect(r).to.have.lengthOf(0)
     })
 
     it('should return empty array with number function', () => {
         const f: IFunc<number> = {x: v => 0, y: v => 0}
-        const r = generateData<number>(0, 0, f)
+        const r = generateData<number>(t0, 0, f)
         expect(r).to.be.an('array')
         expect(r).to.have.lengthOf(0)
     })
@@ -27,21 +31,22 @@ describe('Utils generateData', () => {
         }
         const f: IFunc<IE<boolean>> = {x: v => ({v0: !v, v1: !!v}), y: v => ({v0: !!v, v1: !v})}
 
-        const r = generateData<IE<boolean>>(0, 0, f)
+        const r = generateData<IE<boolean>>(t0, 0, f)
         expect(r).to.be.an('array')
         expect(r).to.have.lengthOf(0)
     })
 
     it('should return one element array with boolean function', () => {
         const f: IFunc<boolean> = {x: v => true, y: v => false}
-        const r = generateData<boolean>(0, 1, f)
+        const r = generateData<boolean>(t0, 1, f)
         expect(r).to.be.an('array')
-        expect(r).to.have.lengthOf(1)
+        expect(r).to.have.lengthOf(r10.length)
+        expect(r).to.deep.equal(r10)
     })
 
     it('should return one element  array with number function', () => {
         const f: IFunc<number> = {x: v => 0, y: v => 0}
-        const r = generateData<number>(0, 1, f)
+        const r = generateData<number>(t0, 1, f)
         expect(r).to.be.an('array')
         expect(r).to.have.lengthOf(1)
     })
@@ -53,7 +58,7 @@ describe('Utils generateData', () => {
         }
         const f: IFunc<IE<boolean>> = {x: v => ({v0: !v, v1: !!v}), y: v => ({v0: !!v, v1: !v})}
 
-        const r = generateData<IE<boolean>>(0, 1, f)
+        const r = generateData<IE<boolean>>(t0, 1, f)
         expect(r).to.be.an('array')
         expect(r).to.have.lengthOf(1)
     })
