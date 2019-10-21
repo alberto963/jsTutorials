b5e938019c614ae5ca7c42d52b827c0f54b0da66
Alberto Petazzi <petazzi63@gmail.com>
2019-10-21 14:24:39 +0200

    rt_32: chg

--- rt_32_reactTestMocha/test/Utils.specs.ts
+++ rt_32_reactTestMocha/test/Utils.specs.ts
@@ -31,4 +31,30 @@ describe('Utils generateData', () => {
         expect(r).to.be.an('array')
         expect(r).to.have.lengthOf(0)
     })
+
+    it('should return one element array with boolean function', () => {
+        const f: IFunc<boolean> = {x: v => true, y: v => false}
+        const r = generateData<boolean>(0, 1, f)
+        expect(r).to.be.an('array')
+        expect(r).to.have.lengthOf(1)
+    })
+
+    it('should return one element  array with number function', () => {
+        const f: IFunc<number> = {x: v => 0, y: v => 0}
+        const r = generateData<number>(0, 1, f)
+        expect(r).to.be.an('array')
+        expect(r).to.have.lengthOf(1)
+    })
+
+    it('should return one element  array with struct type function', () => {
+        interface IE<T> {
+            v0: T
+            v1: T
+        }
+        const f: IFunc<IE<boolean>> = {x: v => ({v0: !v, v1: !!v}), y: v => ({v0: !!v, v1: !v})}
+
+        const r = generateData<IE<boolean>>(0, 1, f)
+        expect(r).to.be.an('array')
+        expect(r).to.have.lengthOf(1)
+    })
 })
