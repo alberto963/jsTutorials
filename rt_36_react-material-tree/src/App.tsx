c75ac0ad20a22d8e0f8f9059a95ac8fcf234a80f
Alberto Petazzi <AP>
2019-11-11 11:18:23 +0100

    rt_36: chg (example working)

--- rt_36_react-material-tree/src/App.tsx
+++ rt_36_react-material-tree/src/App.tsx
@@ -4,13 +4,12 @@ import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
 import Tree from './Tree'
 
-export default function SimpleContainer() {
-  return (
+const SimpleContainer: React.FC = () => 
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth='sm' style={{margin: '100px', padding: '25px', border: 'solid'}}>
         <Typography component={Tree} variant={'button'} />
       </Container>
     </React.Fragment>
-  )
-}
+
+export default SimpleContainer
