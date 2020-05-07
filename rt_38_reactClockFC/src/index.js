574afc94ca35b5bc6760359b801a5c8dcb78c8de
Alberto <petazzi63@gmail.com>
2020-05-07 12:17:40 +0200

    rt_38: chg

--- rt_38_reactClockFC/src/index.js
+++ rt_38_reactClockFC/src/index.js
@@ -1,10 +1,13 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import ReactDOM from 'react-dom'
 
-const DateHeader = ({interval}) => <h2>Hello, world! My interval is {interval} milliseconds</h2>
-const FormattedDate = ({date}) => <h3>It is {date.toLocaleTimeString()}.</h3>
+const DateHeader = ({ interval }) => 
+  <h2>Hello, world! My interval is {interval} milliseconds</h2>
 
-const Clock = ({interval=1000}) => {
+const FormattedDate = ({ date }) => 
+  <h3>It is {date.toLocaleTimeString()}.</h3>
+
+const Clock = ({ interval = 1000 }) => {
 
   const [date, setDate] = useState(new Date())
 
@@ -13,20 +16,20 @@ const Clock = ({interval=1000}) => {
   }, [interval])
 
   return (
-    <div>
+    <>
       <DateHeader interval={interval} />
       <FormattedDate date={date} />
-    </div>
+    </>
   )
 }
 
-const Clocks = ({intervals}) => intervals.map((i, idx) => <Clock interval={i} key={idx}/>)
+const Clocks = ({ intervals }) => intervals.map((i, idx) => <Clock interval={i} key={idx} />)
 
 ReactDOM.render(
-  <div>
-      <Clock />
-      <Clocks intervals={[500, 2000, 4000]} />
-      {[5000, 10000].map((i, idx) => <Clock interval={i} key={idx} />)}
-  </div>,
+  <>
+    <Clock />
+    <Clocks intervals={[500, 2000, 4000]} />
+    {[5000, 10000].map((i, idx) => <Clock interval={i} key={idx} />)}
+  </>,
   document.getElementById('root')
 )
