f44c29135f444b2e34ce179f61d6c1f596402606
apetazzi <Alberto>
2021-06-05 09:14:58 +0200

    rt_24: chg

--- rt_24_reactHook/src/index.js
+++ rt_24_reactHook/src/index.js
@@ -1,5 +1,9 @@
-import React, { useState, useEffect  } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState, useEffect, useCallback, memo } from 'react'
+import ReactDOM, { render } from 'react-dom'
+
+const t = () => Math.floor(Date.now() / 1000)
+const dt = t0 => (t() - t0) + ' :: '
+const t0 = t()
 
 const Example = () => {
   // Declare a new state variable, which we'll call "count"
@@ -21,7 +25,49 @@ const Example = () => {
   )
 }
 
-ReactDOM.render(
-  <Example />,
+// https://dmitripavlutin.com/dont-overuse-react-usecallback/
+
+const MyParent = ({ term }) => {
+  const [count, setCount] = useState(0)
+  
+  console.info(dt(t0) + ` MyParent with term=${term} called`)
+
+  const onItemClickNoCallback = event => console.log('You clicked ', event.currentTarget)
+  const onItemClickCallback = useCallback(event => console.log('You clicked ', event.currentTarget), [term])
+
+// <MyBigListMemoized term={term} id={'no React.callback and React.memo'} onItemClick={onItemClickNoCallback} />
+// <MyBigList term={term} id={'React.callback and no React.memo'} onItemClick={onItemClickCallback} />
+// <MyBigListMemoized term={term} id={'React.callback and React.memo'} onItemClick={onItemClickCallback} />
+
+  return (
+	  <div>
+		<button onClick={() => setCount(count + 1) }>
+        Click me
+        </button>
+		
+		<br />
+		<br />
+        <MyBigListMemoized term={term} id={'React.callback and React.memo'} onItemClick={onItemClickCallback} />
+	  </div>
+	  )
+}
+
+const MyBigList = ({ term, id, onItemClick }) => {
+	console.info(dt(t0) + ` MyBigList with ${id} called`)
+	return (
+		<div>
+		  {Array(term).fill(null).map((item, i) => <span onClick={onItemClick}>{i}</span>)}
+		</div>
+	)
+}
+
+const MyBigListMemoized = memo(MyBigList)
+const MyParentMemoized = memo(MyParent)
+
+render(
+  <div>
+    <Example />
+    <MyParentMemoized term={200000} />
+  </div>,
   document.getElementById('root')
 )
