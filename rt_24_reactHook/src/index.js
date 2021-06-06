26ee31b43d8b2bcfead6dab10d4827d3cd40982a
Alberto <petazzi63@gmail.com>
2021-06-06 12:32:58 +0200

    rt_24: chg

--- rt_24_reactHook/src/index.js
+++ rt_24_reactHook/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, memo } from 'react'
+import React, { useState, useEffect, useCallback, memo, useRef } from 'react'
 import { render } from 'react-dom'
 
 const t = () => Math.floor(performance.now() / 1000)
@@ -30,7 +30,7 @@ const Example = () => {
 const MyParent = ({ term }) => {
   const [count, setCount] = useState(0)
   
-  console.info(dt(t0) + ` MyParent with term=${term} called`)
+  console.log(`${dt(t0)} MyParent with term=${term} called`)
 
   const onItemClick = event => console.log('You clicked ', event.currentTarget)
   const onItemClickCallback = useCallback(onItemClick, [term])
@@ -52,23 +52,77 @@ const MyParent = ({ term }) => {
 }
 
 const MyBigList = ({ term, id, onItemClick }) => {
-	console.info(dt(t0) + ` MyBigList with ${id} called`)
+	console.log(`${dt(t0)} MyBigList with ${id} called`)
   const myList = (
     <div>
       {Array(term).fill(null).map((item, i) => <div key={i} onClick={onItemClick}>{i}</div>)}
     </div>
     )
-  console.info(dt(t0) + ` MyBigList with ${id} built list`)
+  console.log(`${dt(t0)} MyBigList with ${id} built list`)
 
 	return myList
 }
 
 const MyBigListMemoized = memo(MyBigList)
 
+// https://dmitripavlutin.com/react-useref-guide/
+
+const Stopwatch = () => {
+  const timerIdRef = useRef(0)
+  const [count, setCount] = useState(0)
+
+  const startHandler = () => {
+    if (timerIdRef.current) { return }
+    timerIdRef.current = setInterval(() => setCount(c => c + 1), 1000)
+  }
+
+  const stopHandler = () => {
+    clearInterval(timerIdRef.current)
+    timerIdRef.current = 0
+  }
+
+  const resetHandler = () => setCount(0)
+  
+  useEffect(() => {
+    return () => clearInterval(timerIdRef.current)
+  }, [])
+
+  console.log(`${dt(t0)} Stopwatch with timerIdRef ${timerIdRef.current} renders`)
+
+  return (
+    <div>
+      <div>Timer: {count}s</div>
+      <div>
+        <button onClick={startHandler}>Start</button>
+        <button onClick={stopHandler}>Stop</button>
+        <button onClick={resetHandler}>Reset</button>
+      </div>
+    </div>
+  )
+}
+
+const InputFocus = () => {
+  const inputRef = useRef()
+
+  useEffect(() => {
+    // Logs `HTMLInputElement` 
+    console.log(`${dt(t0)} InputFocus with inputRef ${inputRef.current} useEffect`)
+
+    // inputRef.current.focus()
+  }, [])
+
+  // Logs `undefined` during initial rendering
+  console.log(`${dt(t0)} InputFocus with inputRef ${inputRef.current} renders`)
+
+  return <input ref={inputRef} type="text" />
+}
+
 render(
   <div>
     <Example />
-    <MyParent term={1000000} />
+    <Stopwatch />
+    <InputFocus />
+    <MyParent term={10} /> {/* To see effects, use 1000000 as term value */}
   </div>,
   document.getElementById('root')
 )
