4f17605d4a42f33fd61edece7d91403364099511
Alberto <petazzi63@gmail.com>
2022-03-19 19:09:36 +0100

    rt_50: chg added experiment with story

---
+++ rt_50_react_17.0.2/src/utils/1_WhenToUseContext.stories.jsx
@@ -0,0 +1,18 @@
+// To be investigated, not used now
+import Story from 'utils/Story'
+
+import AppNoContext from '../1_AppNoContext';
+import AppWithContext from '../2_AppWithContext';
+
+export default {
+  title: 'AdvancedGuide/3 Context',
+  component: Story,
+  subcomponents: { AppNoContext, AppWithContext },
+  parameters: {
+  },
+};
+
+export const WhenToUseContext = (props) => <Story>
+  <AppNoContext />
+  <AppWithContext />
+</Story>;
