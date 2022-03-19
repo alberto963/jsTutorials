aa89d85c64c6b406289685e24adfb39f5bd34142
Alberto <petazzi63@gmail.com>
2022-03-19 19:27:00 +0100

    rt_50: chg refactoring

--- rt_50_react_17.0.2/src/utils/1_WhenToUseContext.jsx
+++ rt_50_react_17.0.2/src/utils/1_WhenToUseContext.jsx
@@ -4,13 +4,13 @@ import Story from 'utils/Story'
 import AppNoContext from '../1_AppNoContext';
 import AppWithContext from '../2_AppWithContext';
 
-export default {
-  title: 'AdvancedGuide/3 Context',
-  component: Story,
-  subcomponents: { AppNoContext, AppWithContext },
-  parameters: {
-  },
-};
+// export default {
+//   title: 'AdvancedGuide/3 Context',
+//   component: Story,
+//   subcomponents: { AppNoContext, AppWithContext },
+//   parameters: {
+//   },
+// };
 
 export const WhenToUseContext = (props) => <Story>
   <AppNoContext />
