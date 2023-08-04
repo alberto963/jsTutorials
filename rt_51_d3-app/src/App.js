8f9f096afc2286d81ec05170402ab2ce5f585818
apetazzi <petazzi63@gmail.com>
2023-08-04 11:10:35 +0200

    rt_51: chg

--- rt_51_d3-app/src/App.js
+++ rt_51_d3-app/src/App.js
@@ -1,8 +1,16 @@
-import React, { useEffect, useRef } from 'react'
-import { select } from 'd3'
+import React, { useEffect, useRef, useState } from 'react'
+import { select, csv } from 'd3'
+import { chart } from './Chart'
 
 const D3Hello = () => {
   const ref = useRef()
+  const [segments, setSegments] = useState([])
+  const [nodes, setNodes] = useState([])
+
+  useEffect(() => {
+    csv("/segments.csv").then(setSegments)
+    csv("/nodes.csv").then(setNodes)
+  }, [])
 
   useEffect(() => {
     const div = select(ref.current)
@@ -11,7 +19,11 @@ const D3Hello = () => {
     div.append('p').text('Hello from D3')
 
     // if (div._groups[0][0].childElementCount === 0) div.append('p').text('Hello from D3')
-  }, [])
+
+    // const c = chart([], nodes)
+    // div.append(c)
+
+  }, [segments, nodes])
 
   return <div ref={ref} />
 }
